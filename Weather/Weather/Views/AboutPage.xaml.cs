a5fedb3e4aa77459803e5df750b4e1888c5a2ac5
Hanna Tekie <hantek90@gmail.com>
2022-04-12 10:45:36 +0200

    ..

--- Weather/Weather/Views/AboutPage.xaml.cs
+++ Weather/Weather/Views/AboutPage.xaml.cs
@@ -14,17 +14,6 @@ namespace Weather.Views
         {
             InitializeComponent();
 
-            //myGif.Source = $"https://cdn.dribbble.com/users/1797299/screenshots/6722344/vejr-skift.gif";
-                //$"https://cdn.dribbble.com/users/481941/screenshots/9797705/media/d6fc408897d7de56eb9c5fe6e39a1dcc.gif";
-
-            //gif2.Source = $"https://icon-library.com/images/weather-icon-gif/weather-icon-gif-18.jpg";
-            //"https://c.tenor.com/YwLRkfregdsAAAAC/weather-change.gif";
-            //  < Image x: Name = "gif1" Source = "sunnyGif" AbsoluteLayout.LayoutBounds = "1,1, 0,5 " />
-
-            //  < Image x: Name = "gif2" Source = "Gif" VerticalOptions = "End" HorizontalOptions = "End" HeightRequest = "150" ></ Image >
-
-
-
         }
     }
 }
